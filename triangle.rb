0728f206b5bff574322cf98e962b3850368e0ce8
Abdullah <abd00s@users.noreply.github.com>
2015-02-15 10:56:50 -0500

    Complete about_triangles_project_2

--- triangle.rb
+++ triangle.rb
@@ -15,6 +15,9 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
+  if (a <= 0 || b <= 0 || c <= 0) || ((a + b <= c) || (a + c <= b) || (b + c <= a))
+    raise TriangleError, "Lengths cannot form triangle!"
+  end
   if a == b && a == c
     :equilateral
   elsif a == b || a == c || b == c
